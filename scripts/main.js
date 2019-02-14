cbd79655e32640bdd0ad84ea968c436445f92e1f
Dan <dan_oconnor@live.ca>
2019-02-14 16:16:08 -0500

    ��

--- scripts/main.js
+++ scripts/main.js
@@ -1,49 +1,100 @@
-function successFunc(data) {
+const app = {}
+
+app.flickGallery = function () {
+  $('.main-carousel').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true,
+    pageDots: false
+  });
+}
+
+app.successFunc = function (data) {
   console.log(data);
 }
 
-let updateUseraccount
-
-const userCount = () => {
-  var url = "https://sheetsu.com/apis/v1.0su/6dd50df793f9/search";
-  var params = {
-    "item": "ketchupHotdog"
-  };
+app.returnInfo = function (func, itemName, index, column) {
   $.ajax({
-    url: url,
-    data: params,
-    success: successFunc
-  }).then(function (res) {
-    console.log(res[0].userCount);
-    updateUseraccount = parseInt(res[0].userCount);
-    updateUseraccount++
-    console.log(updateUseraccount);
+    url: 'https://sheetsu.com/apis/v1.0su/6dd50df793f9',
+    success: app.successFunc
+  }).then(function (data) {
+    func(itemName, parseInt(data[index][column]) + 1)
   })
-
-  return updateUseraccount
 }
 
-const update = function (param1, param2, param3) {
-
-  var url = `https://sheetsu.com/apis/v1.0bu/6dd50df793f9/${param1}/${param2}`;
-  var params = {
-    userCount: param3
-  };
+app.updateUserCount = function (itemName, newVal) {
   $.ajax({
     type: "PATCH",
-    url: url,
-    data: params,
-    success: successFunc
-  })
-
+    url: `https://sheetsu.com/apis/v1.0bu/6dd50df793f9/item/${itemName}`,
+    data: {
+      "userCount": newVal
+    },
+    success: app.successFunc
+  });
 }
 
-$('.over-rated').on('click', function () {
-  update('item', 'ketchupHotdog', userCount())
-})
+app.updateOverRatedCount = function (itemName, newVal) {
+  $.ajax({
+    type: "PATCH",
+    url: `https://sheetsu.com/apis/v1.0bu/6dd50df793f9/item/${itemName}`,
+    data: {
+      "overatedCount": newVal
+    },
+    success: app.successFunc
+  });
+}
 
-$('.main-carousel').flickity({
-  // options
-  cellAlign: 'left',
-  contain: true
+app.updateUnderRatedCount = function (itemName, newVal) {
+  $.ajax({
+    type: "PATCH",
+    url: `https://sheetsu.com/apis/v1.0bu/6dd50df793f9/item/${itemName}`,
+    data: {
+      "underatedCount": newVal
+    },
+    success: app.successFunc
+  });
+}
+
+app.updateAccuratelyRatedCount = function (itemName, newVal) {
+  $.ajax({
+    type: "PATCH",
+    url: `https://sheetsu.com/apis/v1.0bu/6dd50df793f9/item/${itemName}`,
+    data: {
+      "accuratelyRatedCount": newVal
+    },
+    success: app.successFunc
+  });
+}
+
+app.buttonClick = function () {
+  $('#over-rated').on('click', function () {
+    const currentItem = $(this).attr('class');
+    const currentItemIndex = parseInt($(this).attr('data-index'))
+    app.returnInfo(app.updateOverRatedCount, currentItem, currentItemIndex, "overatedCount")
+    app.returnInfo(app.updateUserCount, currentItem, currentItemIndex, "userCount")
+  })
+
+  $('#under-rated').on('click', function () {
+    const currentItem = $(this).attr('class');
+    const currentItemIndex = parseInt($(this).attr('data-index'))
+    app.returnInfo(app.updateUnderRatedCount, currentItem, currentItemIndex, "underatedCount")
+    app.returnInfo(app.updateUserCount, currentItem, currentItemIndex, "userCount")
+  })
+
+  $('#accurately-rated').on('click', function () {
+    const currentItem = $(this).attr('class');
+    const currentItemIndex = parseInt($(this).attr('data-index'))
+    app.returnInfo(app.updateAccuratelyRatedCount, currentItem, currentItemIndex, "accuratelyRatedCount")
+    app.returnInfo(app.updateUserCount, currentItem, currentItemIndex, "userCount")
+  })
+}
+
+app.init = function () {
+  app.flickGallery();
+  app.buttonClick();
+}
+
+
+$(document).ready(function () {
+  app.init()
 });
