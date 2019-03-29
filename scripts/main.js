01edb57377545566e41992025aef5e3b5c963d72
Dan <dan_oconnor@live.ca>
2019-03-29 16:32:02 -0400

    update js

--- scripts/main.js
+++ scripts/main.js
@@ -7,19 +7,19 @@ app.flickGallery = () => {
     pageDots: false,
     draggable: false
   });
-}
+};
 
-app.successFunc = (data) => {
+app.successFunc = data => {
   // console.log(data);
-}
+};
 
 // Makes a call to API to return data
 app.returnInfo = (func, itemName, index, column) => {
   $.ajax({
     url: 'https://sheetsu.com/apis/v1.0su/6dd50df793f9',
     success: app.successFunc
-  }).then((data) => {
-    // Calling fucntion to return data on users who have made the same choice
+  }).then(data => {
+    // Calling function to return data on users who have made the same choice
     app.returnPercentage(index, column, data);
     // Calling function to record user choice & send back to API to increment
     func(itemName, parseInt(data[index][column]) + 1);
@@ -28,13 +28,29 @@ app.returnInfo = (func, itemName, index, column) => {
 
 // Converts data returned on historical user choices to % & displays on DOM
 app.returnPercentage = (index, column, data) => {
-  let value = Math.floor(parseFloat(data[index].overatedPercent) * 100).toString();
+  console.log(data, index, column);
+  let percentage;
+  if (column === 'overatedCount') {
+    percentage = 'overatedPercent';
+  } else if (column === 'underatedCount') {
+    percentage = 'underRatedPercent';
+  } else {
+    percentage = 'accRatedPercent';
+  }
+  let value = Math.floor(parseFloat(data[index][percentage]) * 100).toString();
   $('.result').html(`
     <p>${value}% of respondents agree with you</p>
     `);
-  $('.result-wrap').hide().fadeIn(1600, 'linear');
+  $('.result-wrap')
+    .hide()
+    .fadeIn(1600, 'linear');
   app.graph(value);
-}
+  if (index === 0) {
+    $('#mojalContainer')
+      .removeClass('mojalHidden')
+      .addClass('mojalShow');
+  }
+};
 
 // Called in app.returnInfo to increment overrated count for each item
 app.updateOverRatedCount = (itemName, newVal) => {
@@ -42,7 +58,7 @@ app.updateOverRatedCount = (itemName, newVal) => {
     type: 'PATCH',
     url: `https://sheetsu.com/apis/v1.0bu/6dd50df793f9/item/${itemName}`,
     data: {
-      'overatedCount': newVal
+      overatedCount: newVal
     },
     success: app.successFunc
   });
@@ -54,7 +70,7 @@ app.updateUnderRatedCount = (itemName, newVal) => {
     type: 'PATCH',
     url: `https://sheetsu.com/apis/v1.0bu/6dd50df793f9/item/${itemName}`,
     data: {
-      'underatedCount': newVal
+      underatedCount: newVal
     },
     success: app.successFunc
   });
@@ -66,7 +82,7 @@ app.updateAccuratelyRatedCount = (itemName, newVal) => {
     type: 'PATCH',
     url: `https://sheetsu.com/apis/v1.0bu/6dd50df793f9/item/${itemName}`,
     data: {
-      'accuratelyRatedCount': newVal
+      accuratelyRatedCount: newVal
     },
     success: app.successFunc
   });
@@ -74,26 +90,41 @@ app.updateAccuratelyRatedCount = (itemName, newVal) => {
 
 // Stores event listeneners on user options
 app.userChoice = () => {
-  $('[data-selection="over-rated"]').on('click', function () {
+  $('[data-selection="over-rated"]').on('click', function() {
     const currentItem = $(this).attr('class');
     const currentItemIndex = parseInt($(this).attr('data-index'));
-    app.returnInfo(app.updateOverRatedCount, currentItem, currentItemIndex, 'overatedCount');
+    app.returnInfo(
+      app.updateOverRatedCount,
+      currentItem,
+      currentItemIndex,
+      'overatedCount'
+    );
     app.hideButtonWrap();
     $('.flickity-prev-next-button.next ').fadeIn(1600, 'linear');
   });
 
-  $('[data-selection="under-rated"]').on('click', function () {
+  $('[data-selection="under-rated"]').on('click', function() {
     const currentItem = $(this).attr('class');
     const currentItemIndex = parseInt($(this).attr('data-index'));
-    app.returnInfo(app.updateUnderRatedCount, currentItem, currentItemIndex, 'underatedCount');
+    app.returnInfo(
+      app.updateUnderRatedCount,
+      currentItem,
+      currentItemIndex,
+      'underatedCount'
+    );
     app.hideButtonWrap();
     $('.flickity-prev-next-button.next ').fadeIn(1600, 'linear');
   });
 
-  $('[data-selection="accurately-rated"]').on('click', function () {
+  $('[data-selection="accurately-rated"]').on('click', function() {
     const currentItem = $(this).attr('class');
     const currentItemIndex = parseInt($(this).attr('data-index'));
-    app.returnInfo(app.updateAccuratelyRatedCount, currentItem, currentItemIndex, 'accuratelyRatedCount');
+    app.returnInfo(
+      app.updateAccuratelyRatedCount,
+      currentItem,
+      currentItemIndex,
+      'accuratelyRatedCount'
+    );
     app.hideButtonWrap();
     $('.flickity-prev-next-button.next ').fadeIn(1600, 'linear');
   });
@@ -106,7 +137,7 @@ app.hideButtonWrap = () => {
 
 // Shows buttons at next slide
 app.showButtonWrap = () => {
-  $('.next').on('click', function () {
+  $('.next').on('click', function() {
     $('.button-wrap').fadeIn(1600, 'linear');
     $('.result-wrap').hide();
     $('.result').html('');
@@ -117,19 +148,21 @@ app.showButtonWrap = () => {
 // Postions arrow in the middle..ish of the img container
 app.calcArrowPosition = () => {
   let $halfImgHeight = $('.carousel-img').height() * 0.5;
-  $('.flickity-prev-next-button.next').hide().css('top', `${$halfImgHeight}px`);
+  $('.flickity-prev-next-button.next')
+    .hide()
+    .css('top', `${$halfImgHeight}px`);
 };
 
 // Ensures arrow is positioned correctly as screen changes
 app.positionArrow = () => {
-  $(window).resize(function () {
+  $(window).resize(function() {
     app.calcArrowPosition();
   });
 };
 
 // Hides arrow after user choice
 app.hideArrow = () => {
-  $('.flickity-prev-next-button.next').on('click', function () {
+  $('.flickity-prev-next-button.next').on('click', function() {
     $('.flickity-prev-next-button.next').fadeOut(1600, 'linear');
   });
 };
@@ -137,21 +170,27 @@ app.hideArrow = () => {
 // Smooth scroll from landing page to carousel -  researched from StackOver Flow
 app.scrollToCarousel = () => {
   // Listens for click on down arrow button
-  $('.down-arrow').on('click', function () {
+  $('.down-arrow').on('click', function() {
     // What I think is happening here is that the distance from the top of the page to the top of the main-carousel is being calculated & then passed as the property to animate, 1600 is the duration of the animation in milliseconds
-    $('html, body').animate({
-      scrollTop: $('.main-carousel').offset().top
-    }, 1600);
+    $('html, body').animate(
+      {
+        scrollTop: $('.main-carousel').offset().top
+      },
+      1600
+    );
   });
 };
 
 // Sets empty graph div to height of the result div & animates the width to the percentage of other user who made the same choice
-app.graph = (value) => {
+app.graph = value => {
   let $resultHeight = $('.result').height();
   $('.graph').css('min-height', `${$resultHeight}px`);
-  $('.graph').animate({
-    width: `${value}%`
-  }, 1800);
+  $('.graph').animate(
+    {
+      width: `${value}%`
+    },
+    1800
+  );
 };
 
 // Initialize event listeners & apps to be called at page load
@@ -163,14 +202,9 @@ app.init = () => {
   app.positionArrow();
   app.hideArrow();
   app.scrollToCarousel();
-}
+};
 
 // Blast off!!
-$(document).ready(function () {
+$(document).ready(function() {
   app.init();
 });
-
-/*
-The API can be found here https://sheetsu.com/apis/v1.0su/6dd50df793f9
-It's not 100% reliable, it sends the data 10 times out of 10 but I would say it updates accuraltely 7 out of 10
-*/
